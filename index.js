8c5d995d4794cc2467f3281514936b59c8132cf6
Mustack <jl.martin1990@gmail.com>
2017-04-21 21:43:13 -0400

    fetch rss feed

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const fetch = require('node-fetch');
+const xml2js = require('xml2js');
+
+const parseString = new xml2js.Parser().parseString;
 
 fetch('http://yourmomshousepodcast.libsyn.com/rss')
 .then(res => res.text())
-.then(x=>console.log(x));
+.then(rssXml => parseString(rssXml))
+.then(rssJson => console.log(rssJson));
