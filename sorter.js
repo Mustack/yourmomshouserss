fad13084c56d23eda57feba224424beea164e7ca
Mustack <jl.martin1990@gmail.com>
2017-04-22 01:09:39 -0400

    almost sorting now

--- sorter.js
+++ sorter.js
@@ -2,10 +2,6 @@ const aGreaterThanB = 1;
 const aLessThanB = -1;
 
 function getNum(episode) {
-  if (episode.title[0].match(/\d+/g).length > 1) {
-    console.error(episode.title[0] + ' Has more than 1 number');
-  }
-
   return Number(episode.title[0].match(/\d+/g)[0]);
 }
 
@@ -24,7 +20,7 @@ module.exports = {
     }
 
     if (a.pubDate[0] !== 'Mon, 17 Oct 2016 00:00:00 +0000' && b.pubDate[0] !== 'Mon, 17 Oct 2016 00:00:00 +0000') {
-      return getNum(a) > getNum(b) ? aGreaterThanB : aLessThanB;
+      return undefined;
     }
   }
 }
